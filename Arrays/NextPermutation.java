c3f1597aa8325652e6619d83447f67a44b9c0251
Aarna Gupta <aarna02gupta@gmail.com>
2026-08-03 22:55:11 +0530

    Implement Next Permutation algorithm with optimal approach

---
+++ Arrays/NextPermutation.java
@@ -0,0 +1,55 @@
+
+public class NextPermutation {
+    public void getNextPermutation(int[] arr) {
+        int n = arr.length;
+        int index = -1;
+
+        for (int i = n - 2; i >= 0; i--) {
+            if (arr[i] < arr[i + 1]) {
+                index = i;
+                break;
+            }
+        }
+
+        // If no index found
+        if (index == -1) {
+            // Reverse the entire array
+            reverse(arr, 0, arr.length - 1);
+            return;
+        }
+
+        // Find just larger element
+        for (int i = n - 1; i > index; i--) {
+            if (arr[i] > arr[index]) {
+                swap(arr, i, index);
+                break;
+            }
+            }
+            // Step 3: reverse everything after index
+        reverse(arr, index + 1, n - 1);
+        }
+        private void reverse (int [] arr, int start, int end){
+            while (start < end) {
+            swap(arr, start, end);
+            start++;
+            end--;
+        }
+    }
+ private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+     public static void main(String[] args) {
+        int[] arr = {1, 5, 8, 4, 7, 6, 5, 3, 1};
+
+        NextPermutation obj = new NextPermutation();
+        obj.getNextPermutation(arr);
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+    
+}
